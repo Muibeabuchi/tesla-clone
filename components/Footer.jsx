9755d01f2d5339d9ae60b7d4f6048c7da2127cd2
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-05-28 01:49:05 +0100

    added cursor pointer to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 w-full text-[#949698] space-y-3 md:space-y-0 h-10 items-center flex flex-col md:flex-row md:justify-center md:space-x-7 md:text-xl">
-      <p>Chiki Tesla © 2023</p>
-      <p>Privacy & Legal</p>
-      <p>Vehicle Recalls</p>
-      <p>News</p>
+    <footer className="absolute  bottom-0 w-full text-[#949698] space-y-3 md:space-y-0 h-10 items-center flex flex-col md:flex-row md:justify-center md:space-x-7 md:text-xl">
+      <p className="cursor-pointer">Chiki Tesla © 2023</p>
+      <p className="cursor-pointer">Privacy & Legal</p>
+      <p className="cursor-pointer">Vehicle Recalls</p>
+      <p className="cursor-pointer">News</p>
     </footer>
   );
 };
